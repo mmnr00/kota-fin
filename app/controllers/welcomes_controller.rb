141986db2393b3b1bdb772fdf37e7ac436ea24dc
Rails Udemy Course <mustakhim.rehan@gmail.com>
2018-09-20 11:30:57 +0800

    Admin Success

---
+++ app/controllers/welcomes_controller.rb
@@ -0,0 +1,6 @@
+class WelcomesController < ApplicationController
+	before_action :authenticate_admin!
+	def index
+
+	end
+end
---
+++ app/models/admin.rb
@@ -0,0 +1,12 @@
+class Admin < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  #attr_accessible :email, :password, :password_confirmation, :remember_me, :username
+
+  validates_presence_of :username, :email
+
+
+end
