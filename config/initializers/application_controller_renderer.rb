fe0d8e0f13be0d7398dd7f34334617ede174757b
Mustakhim Rehan <mmnr00@gmail.com>
2018-11-01 07:30:06 +0800

    Add month name

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -10,3 +10,16 @@
 $billplz_url = "https://billplz-staging.herokuapp.com/api/v3/"
 $billplz = "https://billplz-staging.herokuapp.com/"
 $api_key = "6d78d9dd-81ac-4932-981b-75e9004a4f11"
+$month_name = { 1=>"January",
+								2=>"February",
+								3=>"March",
+								4=>"April",
+								5=>"May",
+								6=>"June",
+								7=>"July",
+								8=>"August",
+								9=>"September",
+								10=>"October",
+								11=>"November",
+								12=>"December",
+							}
