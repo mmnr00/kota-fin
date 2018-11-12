74f693710102255da00cf5db418396d39f22a91a
mmnr00 <mmnr00@gmail.com>
2018-11-12 11:32:02 +0800

    remove -pub

--- config/application.rb
+++ config/application.rb
@@ -10,8 +10,8 @@ module KidcareTaskaV3
   class Application < Rails::Application
     config.load_defaults 5.2
     config.assets.paths << Rails.root.join("vendor","assets", "fonts")
-    config.assets.paths << File.join(Rails.root, '/vendor/webarch-cores')
-    config.assets.paths << File.join(Rails.root, '/vendor/webarch-plugins')
+    #config.assets.paths << File.join(Rails.root, '/vendor/webarch-core')
+    #config.assets.paths << File.join(Rails.root, '/vendor/webarch-plugin')
 
     # Initialize configuration defaults for originally generated Rails version.
     
