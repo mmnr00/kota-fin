ba75a0f48bd1fb9ecdd0a7f9bb36e29946d2903b
mmnr00 <mmnr00@gmail.com>
2018-11-15 19:54:19 +0800

    cap

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -20,7 +20,7 @@
 # role :app, %w{deploy@example.com}, my_property: :my_value
 # role :web, %w{user1@primary.com user2@additional.com}, other_property: :other_value
 # role :db,  %w{deploy@example.com}
-
+config.assets.debug = true
 
 
 # Configuration
