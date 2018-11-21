ff02e5c0eac13e74f5ea8c878ac44ff5fd6453f4
mmnr00 <mmnr00@gmail.com>
2018-11-22 07:37:44 +0800

    remove migration username collegeadmins

--- db/migrate/20181120235623_add_username_to_college_admins.rb
+++
@@ -1,6 +0,0 @@
-class AddUsernameToCollegeAdmins < ActiveRecord::Migration[5.2]
-  def change
-    add_column :college_admins, :username, :string
-    add_index :college_admins, :username, unique: true
-  end
-end
