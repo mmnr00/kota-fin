66ac00574df20e11ace4873cbbd1472bfd23b9c2
mmnr00 <mmnr00@gmail.com>
2018-11-23 17:12:01 +0800

    root_url billplz

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -7,16 +7,23 @@
 #   )
 # end
 
-# BILLPLZ STAGING 
-
-$billplz_url = "https://billplz-staging.herokuapp.com/api/v3/"
-$billplz = "https://billplz-staging.herokuapp.com/"
-$api_key = "6d78d9dd-81ac-4932-981b-75e9004a4f11"
-
-#BILLPLZ PRODUCTION
+# ## BILLPLZ LOCAL
 # $billplz_url = "https://billplz-staging.herokuapp.com/api/v3/"
 # $billplz = "https://billplz-staging.herokuapp.com/"
 # $api_key = "6d78d9dd-81ac-4932-981b-75e9004a4f11"
+# $root_url = "http://localhost:3000/"
+
+## BILLPLZ STAGING 
+$billplz_url = "https://billplz-staging.herokuapp.com/api/v3/"
+$billplz = "https://billplz-staging.herokuapp.com/"
+$api_key = "6d78d9dd-81ac-4932-981b-75e9004a4f11"
+$root_url = "http://kidcare-staging.herokuapp.com/"
+
+# # BILLPLZ PRODUCTION
+# $billplz_url = "https://billplz-staging.herokuapp.com/api/v3/"
+# $billplz = "https://billplz-staging.herokuapp.com/"
+# $api_key = "6d78d9dd-81ac-4932-981b-75e9004a4f11"
+# $root_url = "http://kidcare-prod.herokuapp.com/"
 
 
 $month_name = { 1=>"Jan",
