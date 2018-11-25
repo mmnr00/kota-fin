11719a4bdfe142e3b9fbeabb5d387dde71afe443
mmnr00 <mmnr00@gmail.com>
2018-11-26 06:12:29 +0800

    staging

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -7,17 +7,17 @@
 #   )
 # end
 
-## BILLPLZ LOCAL
-$billplz_url = "https://billplz-staging.herokuapp.com/api/v3/"
-$billplz = "https://billplz-staging.herokuapp.com/"
-$api_key = "6d78d9dd-81ac-4932-981b-75e9004a4f11"
-$root_url = "http://localhost:3000/"
-
-# ## BILLPLZ STAGING 
+# ## BILLPLZ LOCAL
 # $billplz_url = "https://billplz-staging.herokuapp.com/api/v3/"
 # $billplz = "https://billplz-staging.herokuapp.com/"
 # $api_key = "6d78d9dd-81ac-4932-981b-75e9004a4f11"
-# $root_url = "http://kidcare-staging.herokuapp.com/"
+# $root_url = "http://localhost:3000/"
+
+## BILLPLZ STAGING 
+$billplz_url = "https://billplz-staging.herokuapp.com/api/v3/"
+$billplz = "https://billplz-staging.herokuapp.com/"
+$api_key = "6d78d9dd-81ac-4932-981b-75e9004a4f11"
+$root_url = "http://kidcare-staging.herokuapp.com/"
 
 # # BILLPLZ PRODUCTION
 # $billplz_url = "https://billplz-staging.herokuapp.com/api/v3/"
