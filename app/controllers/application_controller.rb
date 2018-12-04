bbc8d6a255947941a3cbe7fa1be33fffbeea097b
mmnr00 <mmnr00@gmail.com>
2018-12-04 16:03:37 +0800

    protect from forgery

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
 	 before_action :configure_permitted_parameters, if: :devise_controller?
+	 protect_from_forgery prepend: true
 
 	 #def current_taska
 	 	#return unless session[:Taska_id]
