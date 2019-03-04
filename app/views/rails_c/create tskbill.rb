f1a891f9234d67fa9858029238d5552f971f4fad
mmnr00 <mmnr00@gmail.com>
2019-03-04 19:51:04 +0800

    taska

--- app/views/rails_c/create tskbill.rb
+++ app/views/rails_c/create tskbill.rb
@@ -5,3 +5,7 @@ Tskbill.create(real: 200, disc: 100, payment_id: 304)
 Tskbill.create(real: 150, disc: 75, payment_id: 88)
 Tskbill.create(real: 150, disc: 75, payment_id: 303)
 Tskbill.create(real: 200, disc: 100, payment_id: 89)
+
+
+# new for neesa
+Tskbill.create(real: 150, disc: 75, payment_id: 301)
