c6f541f3847c88c19a86c07a75b2539517789495
mmnr00 <mmnr00@gmail.com>
2019-03-10 17:04:23 +0800

    foto

---
+++ app/views/rails_c/ctrlogo.rb
@@ -0,0 +1,13 @@
+t=Taska.all
+#t=Taska.where.not(id: [5, 9, 1, 44, 45, 4, 48])
+t.each do |tsk|
+
+if !tsk.fotos.where(foto_name: "COMPANY LOGO").present?
+Foto.create(foto_name: "COMPANY LOGO", taska_id: tsk.id)
+end
+
+if !tsk.tsklvs.where(name: "UNPAID LEAVE").present?
+Tsklv.create(name: "UNPAID LEAVE", desc: "PLEASE EDIT YOUR DESCRIPTION AND NO OF DAYS", day: 15, taska_id: tsk.id)
+end
+
+end
