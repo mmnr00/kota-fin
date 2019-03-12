ad4c6587b96679e44d019a46674c2d801c686576
mmnr00 <mmnr00@gmail.com>
2019-03-12 10:40:21 +0800

    psl

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -25,7 +25,7 @@ def my_expenses
 	if params[:expense][:month].present?
 		mth = params[:expense][:month].to_i
 		year = params[:expense][:year].to_i
-		psldt = Date.new(year,mth) + 1.months
+		psldt = Date.new(year,mth)
 		@taska_payslips = @taska.payslips.where(mth: psldt.month, year: psldt.year)
 		@taska_chart = @taska.expenses.where(month: params[:expense][:month]).where(year: params[:expense][:year]) 
 		@taska_expense = @taska.expenses.where(month: params[:expense][:month]).where(year: params[:expense][:year]).order('UPDATED_AT DESC')
