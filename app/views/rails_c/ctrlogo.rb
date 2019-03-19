989c63c96a496a2d4315259747b2777ecc65baa7
mmnr00 <mmnr00@gmail.com>
2019-03-19 09:09:40 +0800

    taska

--- app/views/rails_c/ctrlogo.rb
+++ app/views/rails_c/ctrlogo.rb
@@ -11,3 +11,13 @@ Tsklv.create(name: "UNPAID LEAVE", desc: "PLEASE EDIT YOUR DESCRIPTION AND NO OF
 end
 
 end
+
+t=Taska.all
+#t=Taska.where.not(id: [5, 9, 1, 44, 45, 4, 48])
+t.each do |tsk|
+
+if !tsk.fotos.where(foto_name: "CENTER HANDBOOK").present?
+Foto.create(foto_name: "CENTER HANDBOOK", taska_id: tsk.id)
+end
+
+end
