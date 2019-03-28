15ebdfa1e1d014d51efe418b6a8151ca398fb2a9
mmnr00 <mmnr00@gmail.com>
2019-03-28 20:47:25 +0800

    tsk

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -29,8 +29,9 @@ def my_expenses
 		@taska_payslips = @taska.payslips.where(mth: psldt.month, year: psldt.year)
 		@taska_chart = @taska.expenses.where(month: params[:expense][:month]).where(year: params[:expense][:year]) 
 		@taska_expense = @taska.expenses.where(month: params[:expense][:month]).where(year: params[:expense][:year]).order('UPDATED_AT DESC')
-		#@taska_payments = @taska.payments.where.not(name: "TASKA PLAN").where('extract(year  from updated_at) = ?', year).where('extract(month  from updated_at) = ?', mth)
-		@taska_payments = @taska.payments.where.not(name: "TASKA PLAN").where(bill_month: params[:expense][:month]).where(bill_year: params[:expense][:year])
+		@taska_payments = @taska.payments.where.not(name: "TASKA PLAN").where('extract(year  from updated_at) = ?', year).where('extract(month  from updated_at) = ?', mth)
+		@payments_due = @taska.payments.where.not(name: "TASKA PLAN").where(bill_month: params[:expense][:month]).where(bill_year: params[:expense][:year])
+		@payments_pie = @payments_due.where(paid: false).or(@taska_payments.where(paid: true))
 		@taska_plan = @taska.payments.where(name: "TASKA PLAN").where(paid: true).where('extract(month from updated_at) = ?', mth).where('extract(year from updated_at) = ?', year)
 	else
 		@taska_payslips = @taska.payslips.where(year: params[:expense][:year])
