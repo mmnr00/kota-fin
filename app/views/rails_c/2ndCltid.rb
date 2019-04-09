044ce30fc48f9e6ec31e8526b36a90a48ae67fa9
mmnr00 <mmnr00@gmail.com>
2019-04-09 20:13:36 +0800

    tsk

--- app/views/rails_c/2ndCltid.rb
+++ app/views/rails_c/2ndCltid.rb
@@ -1,5 +1,5 @@
-tsk=[52,50,49,47,46]
-cltid={52=>"1y3glusm",50=>"uekqxp1s",49=>"uekqxp1s",47=>"nseb_5tl",46=>"nseb_5tl"}
+tsk=[72,66,61,59,55,56,54,53,52,50,49,47,46]
+cltid={72=>"zcwcyfc2",66=>"eaf0w8ws",61=>"rsimya7d",59=>"rmttjlgv",55=>"40bx20ao",56=>"40bx20ao",54=>"kwkamuzp",53=>"h3xuyihc",52=>"1y3glusm",50=>"uekqxp1s",49=>"uekqxp1s",47=>"nseb_5tl",46=>"nseb_5tl"}
 
 Taska.find(tsk).each do |t|
 t.collection_id2 = cltid[t.id]
