36b8ab940747eb30edf2e04208334a7f2096b3f0
mmnr00 <mmnr00@gmail.com>
2019-04-15 05:48:44 +0800

    tsk

--- app/views/rails_c/git push.rb
+++ app/views/rails_c/git push.rb
@@ -13,3 +13,8 @@ git push
 git diff HEAD master_n
 git diff HEAD 1fbc
 git checkout 1fbc
+
+heroku maintenance:on --app kidcare-prod
+dropdb prod-bcup -U MustakhimRehan
+heroku pg:pull DATABASE_URL prod-bcup --app kidcare-prod
+git push prod-heroku production_n:master
