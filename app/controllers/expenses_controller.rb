06bd2e5375f434008656e6581586a7bc45e619de
mmnr00 <mmnr00@gmail.com>
2019-04-20 13:18:32 +0800

    t

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -94,12 +94,16 @@ def my_expenses
 		#TASKA BILLS START
 			#HASH FOR BILLS
 			@bill_hash = Hash.new
+			@partial_hash = Hash.new
+			@unpaid_hash = Hash.new
 			@taska_payments = nil
+			@tot_unpaid = 0.00
 			(1..12).each do |mth|
 				dt = Time.find_zone("Singapore").local(year,mth)
 				payment = @taska.payments.where.not(name: "TASKA PLAN")
 				curr_pmt = payment.where(bill_month: mth).where(bill_year: year)
 				curr_pmt_paid = curr_pmt.where(paid: true)
+				curr_pmt_unpaid = curr_pmt.where(paid: false)
 				#CDTN_1 = current period pay early
 				cdtn_1 = curr_pmt_paid.where("updated_at < ?", dt)
 				#CDTN_2 = current period pay this month
@@ -124,7 +128,31 @@ def my_expenses
 				else
 					@taska_payments = @taska_payments.or(all_payments)
 				end
+				#START PARTIAL
+				#CDTN_1 All partials paid this month or previous month for current month bill
+	      cdtn_1par = 0.00
+	      curr_pmt_unpaid.each do |pmt|
+	        if pmt.parpayms.present?
+	          cdtn_1par += pmt.parpayms.where("upd < ?", dt).sum(:amt) 
+	          cdtn_1par += pmt.parpayms.where('extract(year  from upd) = ?', year).where('extract(month  from upd) = ?', mth).sum(:amt) 
+	        end
+	      end
+	      #CDTN_2 previous months bills paid partially this month
+	      cdtn_2par = 0.00
+	      dt_lp=dt-1.months
+	      while dt_lp >= stp_lp
+	        payment.where(paid: false).where("bill_month = ? AND bill_year = ?", dt_lp.month, dt_lp.year).each do |pmt|
+	          cdtn_2par += pmt.parpayms.where('extract(year  from upd) = ?', year).where('extract(month  from upd) = ?', mth).sum(:amt)
+	        end
+	      dt_lp -= 1.months
+	      end
+	      @partial_hash[mth] = cdtn_1par + cdtn_2par
+				#END PARTIAL
+				@payments_dues = curr_pmt
+				@unpaid_hash[mth] = @payments_dues.where(paid: false).sum(:amount) - cdtn_1par
+				@tot_unpaid += @unpaid_hash[mth]
 			end
+			@bills_partial = 0.00
 			#@taska_payments = @taska.payments.where.not(name: "TASKA PLAN").where(bill_year: year)
 			@payments_due = @taska.payments.where.not(name: "TASKA PLAN").where(bill_year: params[:expense][:year])
 			@payments_pie = @payments_due.where(paid: false).or(@taska_payments.where(paid: true))
