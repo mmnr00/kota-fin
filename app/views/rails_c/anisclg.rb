f26adf60162de343b4f82b15980831061aab2d8c
mmnr00 <mmnr00@gmail.com>
2019-05-05 07:20:18 +0800

    t

--- app/views/rails_c/anisclg.rb
+++ app/views/rails_c/anisclg.rb
@@ -4,3 +4,12 @@ tch_clg = tch.tchdetail_colleges.first
 tch_clg.college_id = 45
 tch_clg.save
 end
+
+  
+tc = Tchdetail.where(ic_1: "680302", ic_2: "10", ic_3: "6532").first
+tc.anis = "true"
+tc.save
+tcl=TchdetailCollege.new
+tcl.tchdetail_id= tc.id 
+tcl.college_id = 46
+tcl.save
