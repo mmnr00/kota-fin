a2f289d7bdf40734d601807c87266c5044d95381
mmnr00 <mmnr00@gmail.com>
2019-05-31 06:08:26 +0800

    tsk

--- app/views/rails_c/git push.rb
+++ app/views/rails_c/git push.rb
@@ -18,7 +18,7 @@ heroku maintenance:on --app kidcare-prod
 #dropdb prod-bcup -U MustakhimRehan
 #heroku pg:pull DATABASE_URL prod-bcup --app kidcare-prod
 git push prod-heroku production_n:master
-heroku run rake db:migrate --app kidcare-prod
+#heroku run rake db:migrate --app kidcare-prod
 heroku restart --app kidcare-prod
 heroku maintenance:off --app kidcare-prod
 
