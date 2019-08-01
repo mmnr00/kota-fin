3f612adf7b72626dc4a33a91f248b95b74554bf7
mmnr00 <mmnr00@gmail.com>
2019-08-01 18:31:22 +0800

    tsk

---
+++ app/views/rails_c/anisatt.rb
@@ -0,0 +1,19 @@
+clg = College.find(51)
+tchds = clg.tchdetails
+
+clg.courses.each do |crs|
+crs.anisprogs.where.not(name: "BREAK").each do |an|
+tchds.each do |tch|
+Anisatt.create(course_id: crs.id, tchdetail_id: tch.id, anisprog_id: an.id, att: true)
+end
+end
+end
+
+clg = College.find(51)
+tchds = clg.tchdetails
+tchds.each do |tch|
+if !tch.post.present?
+tch.post = "CHILDCARE PROVIDER"
+tch.save
+end
+end
