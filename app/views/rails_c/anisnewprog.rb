43a4577ac7a7dd07a728b750aabe34455754b5e8
mmnr00 <mmnr00@gmail.com>
2019-10-20 15:50:04 +0800

    tsk

--- app/views/rails_c/anisnewprog.rb
+++ app/views/rails_c/anisnewprog.rb
@@ -1,12 +1,29 @@
-College.find(57).courses.each do |crs|
+old = 60
+nw = 62
+
+clg=College.find(old)
+clgn = College.find(nw)
+
+clg.courses.each do |crs|
 a = crs.dup
-a.college_id = 60
+a.college_id = nw
+
+if a.name == "DAY 1"
+a.start = clgn.start
+elsif a.name == "DAY 2"
+a.start = clgn.start + 1.days
+elsif a.name == "DAY 3"
+a.start = clgn.end
+end
+
 a.save
+
 crs.anisprogs.each do |ap|
 b = ap.dup
 b.course_id = a.id 
 b.save
 end
+
 end
 
  
