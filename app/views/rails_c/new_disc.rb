66e35df1c1447e79ec882d8a55d6f73f45e7f9d0
mmnr00 <mmnr00@gmail.com>
2019-12-06 17:23:14 +0800

    tsk

--- app/views/rails_c/new_disc.rb
+++ app/views/rails_c/new_disc.rb
@@ -6,16 +6,13 @@ a[55]=2.4/3
 a[56]=2.4/3
 a[61]=2.8/3
 a[66]=2.8/3
-a[72]=2.8/3
+a[72]=1
 a[76]=2.8/3
 a[80]=2.4/3
 a[81]=2.4/3
 
 a.each do |k,v|
-t=Taska.find(k)
-t.discount=v.to_f
+t = Taska.find(k)
+t.discount = v.to_f
 t.save
 end
-
-
-
