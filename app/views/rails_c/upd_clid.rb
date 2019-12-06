7b0c4b3671ba4d31a6e420e5f72116ebb3659691
mmnr00 <mmnr00@gmail.com>
2019-12-06 16:38:05 +0800

    tsk

---
+++ app/views/rails_c/upd_clid.rb
@@ -0,0 +1,14 @@
+t=Taska.find(275)
+t.collection_id="j_yldhkq"
+t.collection_id2="clcddhvr"
+t.save
+
+t=Taska.find(278)
+t.collection_id="odyzqnth"
+t.collection_id2="pq4ou0ad"
+t.save
+
+t=Taska.find(282)
+t.collection_id="blpoa1sd"
+t.collection_id2="_yoitbi9"
+t.save
