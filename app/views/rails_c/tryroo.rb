44e343142f399eaa3f72e6f7c2e61ecf64a89830
mmnr00 <mmnr00@gmail.com>
2020-04-15 06:13:39 +0800

    tsk

--- app/views/rails_c/tryroo.rb
+++ app/views/rails_c/tryroo.rb
@@ -1,8 +1,11 @@
-require 'roo'
-xlsx = Roo::Spreadsheet.open('./tryroo.xlsx')
-header = xlsx.row(xlsx.first_row)
-((xlsx.first_row+1)..(xlsx.last_row)).each do |n|
+#require 'roo'
+xlsx = Roo::Spreadsheet.open('./Borang_JMB.xlsx')
+header = xlsx.row(xlsx.first_row+2)
+((xlsx.first_row+4)..(xlsx.last_row)).each do |n|
 xlsx.row(n)
 row = Hash[[header, xlsx.row(n)].transpose]
-Classroom.create(classroom_name: row["NAME"], taska_id: row["TASKA"], base_fee: row["BASE FEE"])
+if row["BLOCK/ROAD"].present?
+puts row["OWNER DETAILS"]
+end
+#Classroom.create(classroom_name: row["NAME"], taska_id: row["TASKA"], base_fee: row["BASE FEE"])
 end
