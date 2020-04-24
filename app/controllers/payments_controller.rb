44d5416e07d5383dd14dfda32358020f1443d57e
mmnr00 <mmnr00@gmail.com>
2020-04-24 17:17:28 +0800

    tsk

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -62,8 +62,8 @@ class PaymentsController < ApplicationController
                               #:callback_url=>  "YOUR RETURN URL"}.to_json,
                   :basic_auth => { :username => ENV['BILLPLZ_APIKEY'] },
                   :headers => { 'Content-Type' => 'application/json', 'Accept' => 'application/json' })
-          render json: data_billplz and return
-          #data = JSON.parse(data_billplz.to_s)
+          #render json: data_billplz and return
+          data = JSON.parse(data_billplz.to_s)
           #CREATE PAYMENT
           if data["id"].present?
             @payment = Payment.new
@@ -157,7 +157,7 @@ class PaymentsController < ApplicationController
     if tot > 0
       flash[:success] = "Complete bill for #{@taska.booking.to_i} months"
     else
-      flash[:success] = "Please update Bill Item in Fee Section"
+      flash[:danger] = "Please update Bill Item in Fee Section"
     end
     redirect_to taskashow_path(@taska)
   end
