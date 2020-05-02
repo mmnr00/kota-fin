3a9899971ceebb657135812f44ea2cf98834b7c4
mmnr00 <mmnr00@gmail.com>
2020-05-02 08:16:49 +0800

    tsk

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -155,7 +155,7 @@ class PaymentsController < ApplicationController
     end # end classroom
 
     if tot > 0
-      flash[:success] = "#{no_bill} Bills Created"
+      flash[:success] = "Bills Creation Successful"
     else
       flash[:danger] = "Please update Bill Item in Fee Section"
     end
