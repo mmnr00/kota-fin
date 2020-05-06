09078873f42e828eb5d33dfff5e5b581a44de0a0
mmnr00 <mmnr00@gmail.com>
2020-05-06 11:28:57 +0800

    tsk

---
+++ gt-prd.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+git checkout dev
+git add -A
+git commit -m "tsk"
+git push
+
+git checkout staging
+git merge dev
+git push
+
+git checkout t-prod
+git merge staging
+git push
+
+git diff HEAD staging
+git diff HEAD dev
+git checkout dev
